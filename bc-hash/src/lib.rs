6e18e15693684991f2b26e442079587a9c54faa7
PopcornPaws <lipschitz77@gmail.com>
2025-06-18 14:50:28 +0200

    Added sha2 hashing.

--- bc-hash/src/lib.rs
+++ bc-hash/src/lib.rs
@@ -8,11 +8,12 @@ pub mod hmac;
 #[cfg(feature = "password")]
 pub mod password;
 
+#[cfg(any(feature = "sha2", feature = "sha3"))]
 macro_rules! hash {
-    ($name:ident, $fun:ident, $len:expr) => {
+    ($package:ident, $name:ident, $fun:ident, $len:expr) => {
         pub fn $fun<T: AsRef<[u8]>>(input: T) -> [u8; $len] {
-            use sha3::Digest;
-            sha3::$name::new()
+            use $package::Digest;
+            $package::$name::new()
                 .chain_update(input.as_ref())
                 .finalize()
                 .into()
@@ -20,7 +21,16 @@ macro_rules! hash {
     };
 }
 
-hash!(Sha3_256, sha3_256, 32);
-hash!(Sha3_512, sha3_512, 64);
-hash!(Keccak256, keccak256, 32);
-hash!(Keccak512, keccak512, 64);
+#[cfg(feature = "sha2")]
+hash!(sha2, Sha256, sha2_256, 32);
+#[cfg(feature = "sha2")]
+hash!(sha2, Sha512, sha2_512, 64);
+
+#[cfg(feature = "sha3")]
+hash!(sha3, Sha3_256, sha3_256, 32);
+#[cfg(feature = "sha3")]
+hash!(sha3, Sha3_512, sha3_512, 64);
+#[cfg(feature = "sha3")]
+hash!(sha3, Keccak256, keccak256, 32);
+#[cfg(feature = "sha3")]
+hash!(sha3, Keccak512, keccak512, 64);
